0041bc0d58af13fd960f117e41f7234b1b707b09
pavel <natitshow@gmail.com>
2020-10-28 23:13:12 +0300

    Second  task

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,9 @@ public class MainClassTest extends MainClass {
         Assert.assertTrue("Local number != 14!!!", getLocalNumber() == 14);
     }
 
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertEquals("Method returns less then 45", getClass_number() > 45);
+    }
+
 }
